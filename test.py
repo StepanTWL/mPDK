d5d5400444222e9801ca93402fbedbebfce96bb1
StepanTWL <h3xp3ctb@gmail.com>
2023-03-18 00:29:08 +0200

    18.03.2023 home

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
+import datetime
+
 fix_error = []
 
 
 def parse_answer(package: bytearray, rules: dict) -> list:
-    global fix_error
+    frame = []
     for i in rules.items():
         adr = i[0][:i[0].find('.')]
         bit = i[0][-1]
         val = i[1]
-        if package[int()] & bit == val
+        if package[int()] & bit == val:
+            t = datetime.time(datetime.now())
+            frame.append(f'{t} Байт {adr}, бит {bit} - {val}')
     return frame
 
 answer = bytearray(b'\x7c\xe6\x2e\x06')
